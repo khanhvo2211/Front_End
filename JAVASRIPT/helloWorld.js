0485af918e6aa9dbecbd482ea2e1587797964170
anhkhanh <anhkhanh3348@gmail.com>
2022-09-08 18:45:41 +0700

    edit js

--- JAVASRIPT/helloWorld.js
+++ JAVASRIPT/helloWorld.js
@@ -1,8 +1,4 @@
-const getAllDataFromDB = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    var database = ["hanoi", "hochiminh", "danang"];
-    resolve(database);
-    console.log("getting data from database");
-  }, 3000);
-});
-getAllDataFromDB.then((data) => console.log("data", data));
+function display() {
+  alert("EROOR");
+}
+display();
